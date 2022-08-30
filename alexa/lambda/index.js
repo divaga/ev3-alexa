0b017906ea2f0402d8822996f51e38c6af38ecad
Ivan <13087388+divaga@users.noreply.github.com>
2022-08-30 23:21:00 +0800

    Add files via upload

---
+++ alexa/lambda/index.js
@@ -0,0 +1,248 @@
+/* *
+ * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+ * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+ * session persistence, api calls, and more.
+ * */
+const Alexa = require('ask-sdk-core');
+const axios = require("axios");
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Welcome to AWS Robot Challenge. Please move the robot to drop area and get the souvenir from us!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .withSimpleCard('AWS Robot Challenge', speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const MoveRobotIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'MoveRobotIntent';
+    },
+    handle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        let slotValues = getSlotValues(request.intent.slots); 
+        
+        let direction;
+        let duration;
+        //   SLOT: direction 
+        if (slotValues.direction.heardAs && slotValues.direction.heardAs !== '') {
+            direction = slotValues.direction.heardAs;
+        } else {
+            return responseBuilder
+            .speak('Please specify direction')
+            .withSimpleCard('AWS Robot Challenge', 'Please specify direction')
+            .reprompt('Please specify direction')
+            .getResponse();
+        }
+        
+        //   SLOT: duration 
+        if (slotValues.duration.heardAs && slotValues.duration.heardAs !== '') {
+            duration = slotValues.duration.heardAs;
+        } else {
+            return responseBuilder
+            .speak('Please specify duration')
+            .withSimpleCard('AWS Robot Challenge', 'Please specify duration')
+            .reprompt('please specify duration')
+            .getResponse();
+        }
+        
+        const speechText = 'OK, going ' + direction + ' for ' + duration + ' second';
+        const payload = {"direction_data":direction,"duration_data":duration}
+        
+        axios.post('https://<your-api-gateway>.execute-api.us-east-1.amazonaws.com/dev/{proxy+}',payload)
+            .then(response => {
+                console.log(response)
+                return response
+            })
+            .catch(error => {
+                console.log(error)
+                return error
+            })
+        
+        
+        
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('AWS Robot Challenge', speechText)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+function getSlotValues(filledSlots) { 
+    const slotValues = {}; 
+ 
+    Object.keys(filledSlots).forEach((item) => { 
+        const name  = filledSlots[item].name; 
+ 
+        if (filledSlots[item] && 
+            filledSlots[item].resolutions && 
+            filledSlots[item].resolutions.resolutionsPerAuthority[0] && 
+            filledSlots[item].resolutions.resolutionsPerAuthority[0].status && 
+            filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) { 
+            switch (filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) { 
+                case 'ER_SUCCESS_MATCH': 
+                    slotValues[name] = { 
+                        heardAs: filledSlots[item].value, 
+                        resolved: filledSlots[item].resolutions.resolutionsPerAuthority[0].values[0].value.name, 
+                        ERstatus: 'ER_SUCCESS_MATCH' 
+                    }; 
+                    break; 
+                case 'ER_SUCCESS_NO_MATCH': 
+                    slotValues[name] = { 
+                        heardAs: filledSlots[item].value, 
+                        resolved: '', 
+                        ERstatus: 'ER_SUCCESS_NO_MATCH' 
+                    }; 
+                    break; 
+                default: 
+                    break; 
+            } 
+        } else { 
+            slotValues[name] = { 
+                heardAs: filledSlots[item].value || '', // may be null 
+                resolved: '', 
+                ERstatus: '' 
+            }; 
+        } 
+    }, this); 
+ 
+    return slotValues; 
+}
+
+
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can move the robot by saying move forward, move backward, turn right or turn left for how many second';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .withSimpleCard('AWS Robot Challenge', speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .withSimpleCard('AWS Robot Challenge', speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
+ * It must also be defined in the language model (if the locale supports it)
+ * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * */
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .withSimpleCard('AWS Robot Challenge', speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .withSimpleCard('AWS Robot Challenge', speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        MoveRobotIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler)
+    .addErrorHandlers(
+        ErrorHandler)
+    .withCustomUserAgent('sample/hello-world/v1.2')
+    .lambda();
